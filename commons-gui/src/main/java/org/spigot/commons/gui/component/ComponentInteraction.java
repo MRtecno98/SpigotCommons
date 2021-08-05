5bf330fad3f5cbca0db0a9e1331b012bf6ad854a
MRtecno98 <mr.tecno98@gmail.com>
2021-08-06 01:07:24 +0200

    Switched component interaction
    Now it gives quite more information about the click

--- commons-gui/src/main/java/org/spigot/commons/gui/component/ComponentInteraction.java
+++ commons-gui/src/main/java/org/spigot/commons/gui/component/ComponentInteraction.java
@@ -1,9 +1,24 @@
 package org.spigot.commons.gui.component;
 
-public enum ComponentInteraction {
-	CLICK,
-	RIGHT_CLICK,
-	MIDDLE_CLICK,
-	SHIFT_CLICK,
-	SLOT_CLICK;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.inventory.InventoryView;
+import org.bukkit.inventory.ItemStack;
+import org.spigot.commons.gui.inventory.CartesianInventory;
+import org.spigot.commons.gui.inventory.Vector;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.Wither;
+
+// @With appears not to be working
+@Wither
+@Getter
+@AllArgsConstructor
+@SuppressWarnings("deprecation")
+public class ComponentInteraction {
+	private CartesianInventory inventory;
+	private InventoryView view;
+	private ClickType clickType;
+	private Vector location;
+	private ItemStack item;
 }
