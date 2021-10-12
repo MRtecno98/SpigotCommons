a320916a8b1c08177e03d5588c92dab824f47fc4
MRtecno98 <mr.tecno98@gmail.com>
2021-10-12 22:31:25 +0200

    Added documentation to Triplet

--- commons-util/src/main/java/org/spigot/commons/util/Triplet.java
+++ commons-util/src/main/java/org/spigot/commons/util/Triplet.java
@@ -2,6 +2,16 @@ package org.spigot.commons.util;
 
 import lombok.Value;
 
+/**
+ * Represents three different value in a single object
+ * 
+ * @author MRtecno98
+ * @since 2.0.0
+ *
+ * @param <A> Type of the first element
+ * @param <B> Type of the second element
+ * @param <C> Type of the third element
+ */
 @Value
 public class Triplet<A, B, C> {
 	private A a;
